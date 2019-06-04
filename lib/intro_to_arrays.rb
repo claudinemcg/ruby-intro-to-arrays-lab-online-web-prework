bc1d4cf9a4b25a68c81054b91064fa50692c31ab
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-04 15:38:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,5 +6,17 @@ def array_with_two_elements
   arr = ["Martin", "Claudine"]
 end
 
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
 
   
